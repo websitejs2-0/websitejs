4625b69d0bd34827eadc499a5ac948f48393125c
Rocco Janse <rocco.janse@valtech.nl>
2017-07-29 11:55:09 +0200

    updated venor watch task

--- Frontend/gulp/tasks/vendor.js
+++ Frontend/gulp/tasks/vendor.js
@@ -1,11 +1,10 @@
 var path = require('path'),
     config = require(path.join('..', '..', 'project.config.js')),
+    reload = require('require-again'),
     del = require('del'),
     chalk = require('chalk'),
     gulp = require('gulp'),
     gutil = require('gulp-util'),
-    cached = require('gulp-cached'),
-    remember = require('gulp-remember'),
     filter = require('gulp-filter'),
     sourcemaps = require('gulp-sourcemaps'),
     uglify = require('gulp-uglify'),
@@ -17,13 +16,28 @@ var path = require('path'),
     rename = require('gulp-rename');
 
 /**
- * Cleans compiled vendor files.
+ * Cleans compiled vendor scripts files.
  * @param {function} done Callback.
  */
-function clean(done) {
+function cleanVendorScripts(done) {
     del([
-        path.join(config.folders.vendor.build.root, '**/*.*'), '!' + config.folders.vendor.build.root,
+        //path.join(config.folders.vendor.build.root, '**/*.*'), '!' + config.folders.vendor.build.root,
         path.join(config.folders.vendor.build.js, config.vendorFileName + '.{min.js,min.js.map,js}'), '!' + config.folders.vendor.build.js,
+    ]).then(function(paths) {
+        if (config.debugMode && paths.length > 0) {
+            gutil.log('Cleaned:\n', paths.join('\n'));
+        }
+        done();
+    });
+}
+
+/**
+ * Cleans compiled vendor style files.
+ * @param {function} done Callback.
+ */
+function cleanVendorStyles(done) {
+    del([
+        //path.join(config.folders.vendor.build.root, '**/*.*'), '!' + config.folders.vendor.build.root,
         path.join(config.folders.vendor.build.css, config.vendorFileName + '.{min.css,min.css.map,css}'), '!' + config.folders.vendor.build.css
     ]).then(function(paths) {
         if (config.debugMode && paths.length > 0) {
@@ -54,12 +68,10 @@ function compileVendorScripts(done) {
 
     gulp.src(sources)
         .pipe(sourcemaps.init())
-        .pipe(cached('vendorscripts'))
         .pipe(filterMinified)
         .pipe(uglify())
         .pipe(filterMinified.restore)
         .pipe(strip())
-        .pipe(remember('vendorscripts'))
         .pipe(concat(config.vendorFileName + '.js'))
         .pipe(rename({
             suffix: '.min'
@@ -89,7 +101,6 @@ function compileVendorStyles(done) {
 
     gulp.src(sources)
         .pipe(sourcemaps.init())
-        .pipe(cached('vendorstyles'))
         .pipe(filterMinified)
         .pipe(postcss([
             autoprefixer(),
@@ -106,15 +117,70 @@ function compileVendorStyles(done) {
         ]))
         .pipe(filterMinified.restore)
         .pipe(strip())
-        .pipe(remember('vendorstyles'))
         .pipe(concat(config.vendorFileName + '.min.css'))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(config.folders.vendor.build.css))
         .on('finish', function() { done(); });
 }
 
+/**
+ * Watches vondors folder and configuration for changes.
+ */
+function watchVendor() {
+
+    var configWatcher = gulp.watch(path.join(config.folders.vendor.src.root, 'vendor.config.js'), { ignorePermissionErrors: true });
+    configWatcher.on('all', function() {
+
+        // reload only vendor config (reload pkg needs relative path from working dir)
+        var vendor = reload(path.join('..', '..', 'vendor', 'vendor.config.js'));
+        config.vendor = vendor;
+
+        gutil.log(chalk.white('Updating vendor packages...'));
+        cleanVendorScripts(function() {
+            compileVendorScripts(function() {
+                gutil.log(chalk.yellow('Updated vendor scripts.'));
+            });
+        });
+        cleanVendorStyles(function() {
+            compileVendorStyles(function() {
+                gutil.log(chalk.yellow('Updated vendor styles.'));
+            });
+        });
+    });
+
+    var foldersWatcher = gulp.watch([path.join(config.folders.vendor.src.root), '!' + path.join(config.folders.vendor.src.root, 'vendor.config.js')], { ignorePermissionErrors: true });
+
+    foldersWatcher.on('all', function(e, filePath) {
+
+        if (e === 'add' || e === 'change' || e === 'unlink') {
+            
+            gutil.log(chalk.white('Updating vendor packages...'));
+            
+            if (path.parse(filePath).ext === '.js') {
+                cleanVendorScripts(function() {
+                    compileVendorScripts(function() {
+                        gutil.log(chalk.yellow('Updated vendor scripts.'));
+                    });
+                });                
+            }
+            if (path.parse(filePath).ext === '.css') {
+                cleanVendorStyles(function() {
+                    compileVendorStyles(function() {
+                        gutil.log(chalk.yellow('Updated vendor styles.'));
+                    });
+                });                
+            }
+        }
+    });
+}
+
 // define tasks and task information
-gulp.task('vendor', gulp.series(clean, gulp.parallel(compileVendorScripts, compileVendorStyles)));
+gulp.task('vendor', gulp.series(gulp.parallel(cleanVendorScripts, cleanVendorStyles), gulp.parallel(compileVendorScripts, compileVendorStyles)));
 var vendor = gulp.task('vendor');
 vendor.displayName = 'vendor';
 vendor.description = 'Creates compiled vendor file.';
+
+gulp.task('vendor:watch', gulp.parallel(watchVendor));
+var vendorWatch = gulp.task('vendor:watch');
+vendorWatch.displayName = 'vendor:watch';
+vendorWatch.description = 'Watches vendor config and folders for changes.';
